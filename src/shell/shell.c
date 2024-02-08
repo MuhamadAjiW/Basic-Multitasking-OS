9749b92e4ac7b05a024ff9492bb79f8a59182f35
MuhamadAjiW <16521119@mahasiswa.itb.ac.id>
2024-02-08 13:52:05 +0700

    refactor: used strings instead of printing, reduced windows updates

--- src/shell/shell.c
+++ src/shell/shell.c
@@ -1,6 +1,7 @@
 #include "lib-header/stdtype.h"
 #include "lib-header/syscall.h"
 #include "lib-header/stdio.h"
+#include "lib-header/iostream.h"
 #include "lib-header/stdlib.h"
 #include "lib-header/string.h"
 #include "lib-header/stdmem.h"
@@ -136,7 +137,6 @@ void app_play_animation(char* path){
         }
 
         anim_delete(&anim);
-        print("\n");
         cursor_on();
     }
 }
@@ -161,6 +161,9 @@ void grid_write(){
         }
     }
     cursor_show();
+
+    window_update(&(sh.winfo));
+
     sh.cursor_blocked_2 = 0;
 }
 
@@ -179,7 +182,6 @@ void cursor_hide(){
             window_draw_pixel(&(sh.winfo), (sh.finfo.height * sh.cursor_y) + j, (sh.finfo.width * sh.cursor_x), sh.cursor_background_buffer[j]);
         }
 
-        window_update(&(sh.winfo));
         sh.cursor_blocked_1 = 0;
     }
 }
@@ -194,7 +196,6 @@ void cursor_show(){
             window_draw_pixel(&(sh.winfo), (sh.finfo.height * sh.cursor_y) + j, (sh.finfo.width * sh.cursor_x), sh.default_cursor_color);
         }
 
-        window_update(&(sh.winfo));
         sh.cursor_blocked_1 = 0;
     }
 }
@@ -310,6 +311,7 @@ void cursor_blinking(){
             else{
                 cursor_show();
             }
+            window_update(&(sh.winfo));
             sh.cursor_counter = 0;
         }
         else{
@@ -371,8 +373,6 @@ void shell_clear(){
     if(sh.cursor_on){
         cursor_set(0, 0);
     }
-
-    window_update(&(sh.winfo));
 }
 
 void shell_backspace(){
@@ -410,10 +410,14 @@ void shell_backspace(){
 }
 
 void shell_newline(){
-    print("\n");
-    print(sh.dir.path);
-    print("> ");
+    string_t string = str_new("\n");
+    str_add(&string, sh.dir.path);
+    str_add(&string, "> ");
+
+    print(string.content);
     cursor_limit(sh.cursor_x, sh.cursor_y);
+    
+    str_delete(&string);
 }
 
 void shell_evaluate(){
@@ -421,10 +425,12 @@ void shell_evaluate(){
 
     parser_parse(&sh_parser, sh.reader.buffer_addr, ' ');
 
+    string_t message = str_new("");
+    
     if (sh_parser.word_count > 0){
         if(strcmp(sh_parser.content[0], "clear") == 0){
             if(sh_parser.word_count == 1) shell_clear();
-            else print("\nclear: Invalid argument");
+            else str_add(&message, "\nclear: Invalid argument");
         }
         else if(strcmp(sh_parser.content[0], "dir") == 0){
             dir(sh.dir.cluster_number);
@@ -438,14 +444,14 @@ void shell_evaluate(){
             else if (is_directorypath_valid(sh_parser.content[1], sh.dir.cluster_number)){
                 cd(sh_parser.content[1], &sh.dir);
             } else {
-                print("\ncd: No such directory: ");
-                print(sh_parser.content[1]);
-                print("\n");
+                str_add(&message, "\ncd: No such directory: ");
+                str_add(&message, sh_parser.content[1]);
+                str_addc(&message, '\n');
             }
         }
         else if(strcmp(sh_parser.content[0], "mkdir") == 0){
             if(sh_parser.word_count != 2){
-                print("\nmkdir: Invalid command\n");
+                str_add(&message, "\nmkdir: Invalid command\n");
             }
             else{
                 mkdir(sh_parser.content[1], sh.dir.cluster_number);
@@ -461,9 +467,9 @@ void shell_evaluate(){
             else if (is_directorypath_valid(sh_parser.content[1], sh.dir.cluster_number)){
                 ls(path_to_cluster(sh_parser.content[1], sh.dir.cluster_number));
             } else {
-                print("\nls: ");
-                print(sh_parser.content[1]);
-                print(": No such file or directory\n");
+                str_add(&message, "\nls: ");
+                str_add(&message, sh_parser.content[1]);
+                str_add(&message, ": No such file or directory\n");
             }  
         }
         else if (strcmp(sh_parser.content[0], "rm") == 0) {
@@ -481,21 +487,21 @@ void shell_evaluate(){
             } else if (is_filepath_valid(sh_parser.content[1], sh.dir.cluster_number)){
                 cat(sh.dir.cluster_number);
             } else if (is_directorypath_valid(sh_parser.content[1], sh.dir.cluster_number)){
-                print("\ncat: ");
-                print(sh_parser.content[1]);
-                print(": Is a directory\n");
+                str_add(&message, "\ncat: ");
+                str_add(&message, sh_parser.content[1]);
+                str_add(&message, ": Is a directory\n");
             } else {
-                print("\ncat: ");
-                print(sh_parser.content[1]);
-                print(": No such file\n");
+                str_add(&message, "\ncat: ");
+                str_add(&message, sh_parser.content[1]);
+                str_add(&message, ": No such file\n");
             }
         }
         else if(strcmp(sh_parser.content[0], "changebg") == 0){
             if(sh_parser.word_count == 1){
-                print("\nchangebg: No image provided\n");
+                str_add(&message, "\nchangebg: No image provided\n");
             }
             else if (sh_parser.word_count > 2){
-                print("\nchangebg: Invalid argument\n");
+                str_add(&message, "\nchangebg: Invalid argument\n");
             }
             else{
                 if(is_filepath_valid(sh_parser.content[1], sh.dir.cluster_number)){
@@ -516,22 +522,22 @@ void shell_evaluate(){
                         }
                     }
                     
-                    if(!valid) print("\nchangebg: Invalid file\n");
+                    if(!valid) str_add(&message, "\nchangebg: Invalid file\n");
                     else app_change_background(sh_parser.content[1]);
                 }
                 else{
-                    print("\nchangebg: No such file: ");
-                    print(sh_parser.content[1]);
-                    print("\n");
+                    str_add(&message, "\nchangebg: No such file: ");
+                    str_add(&message, sh_parser.content[1]);
+                    str_addc(&message, '\n');
                 }
             }
         }
         else if(strcmp(sh_parser.content[0], "playanim") == 0){
             if(sh_parser.word_count == 1){
-                print("\nplayanim: No animation provided\n");
+                str_add(&message, "\nplayanim: No animation provided\n");
             }
             else if (sh_parser.word_count > 2){
-                print("\nplayanim: Invalid argument\n");
+                str_add(&message, "\nplayanim: Invalid argument\n");
             }
             else{
                 if(is_filepath_valid(sh_parser.content[1], sh.dir.cluster_number)){
@@ -552,16 +558,16 @@ void shell_evaluate(){
                         }
                     }
                     
-                    if(!valid) print("\nplayanim: Invalid file\n");
+                    if(!valid) str_add(&message, "\nplayanim: Invalid file\n");
                     else{
                         print("\nLoading animation...");
                         app_play_animation(sh_parser.content[1]);
                     }
                 }
                 else{
-                    print("\nplayanim: No such file: ");
-                    print(sh_parser.content[1]);
-                    print("\n");
+                    str_add(&message, "\nplayanim: No such file: ");
+                    str_add(&message, sh_parser.content[1]);
+                    str_addc(&message, '\n');
                 }
             }
         }
@@ -572,17 +578,17 @@ void shell_evaluate(){
             switch (sh_parser.word_count){
             case 3:
                 if(!int_parse_string_valid(sh_parser.content[2])){
-                    print("\nexec: ");
-                    print(sh_parser.content[3]);
-                    print(": Invalid execution amount\n");
+                    str_add(&message, "\nexec: ");
+                    str_add(&message, sh_parser.content[3]);
+                    str_add(&message, ": Invalid execution amount\n");
                     break;
                 }
                 
                 execution_num = int_parse_string(sh_parser.content[2]);
                 if(execution_num < 1){
-                    print("\nexec: ");
-                    print(sh_parser.content[3]);
-                    print(": Invalid execution amount\n");
+                    str_add(&message, "\nexec: ");
+                    str_add(&message, sh_parser.content[3]);
+                    str_add(&message, ": Invalid execution amount\n");
                     break;
                 }
                 /* fall through */
@@ -599,48 +605,53 @@ void shell_evaluate(){
                         }
                     }
                     else{
-                        print("\nexec: Invalid file type");
+                        str_add(&message, "\nexec: Invalid file type");
                     }
                 }
                 else{
-                    print("\nexec: ");
-                    print(sh_parser.content[1]);
-                    print(": No such file\n");
+                    str_add(&message, "\nexec: ");
+                    str_add(&message, sh_parser.content[1]);
+                    str_add(&message, ": No such file\n");
                 }
                 break;
             
             default:
-                print("\nexec: Invalid argument");
+                str_add(&message, "\nexec: Invalid argument");
                 break;
             }
         }
         else if(strcmp(sh_parser.content[0], "ps") == 0){
             if(sh_parser.word_count == 1) ps();
-            else print("\nps: Invalid argument");
+            else str_add(&message, "\nps: Invalid argument");
         }
         else if(strcmp(sh_parser.content[0], "kill") == 0){
             if(sh_parser.word_count == 2){
                 if(int_parse_string_valid(sh_parser.content[1])){
                     uint32_t pid = int_parse_string(sh_parser.content[1]);
                     if(pid == 0){
-                        print("\nkill: Terminating the kernel is not allowed");
+                        str_add(&message, "\nkill: Terminating the kernel is not allowed");
                     } else{
                         kill(pid);
                     }
                 } else{
-                    print("\nkill: Invalid argument");
+                    str_add(&message, "\nkill: Invalid argument");
                 }
             } else{
-                print("\nkill: Invalid argument");
+                str_add(&message, "\nkill: Invalid argument");
             }
         }
         else{
-            print("\nNo Command found: ");
-            print(sh.reader.buffer_addr);
-            print("\n");
+            str_add(&message, "\nNo Command found: ");
+            str_add(&message, sh.reader.buffer_addr);
+            str_addc(&message, '\n');
         }
     }
 
+    sout_t outstream = sout_newstr(message);
+    sout_printall_ws(&outstream);
+
+    str_delete(&message);
+    sout_clear(&outstream);
     parser_clear(&sh_parser);
 }
 
@@ -665,12 +676,14 @@ int main(void) {
             case LARROW_CHAR:
                 if(cursor_move(-1)){
                     reader_move(-1);
+                    window_update(&(sh.winfo));
                 }
                 break;
 
             case RARROW_CHAR:
                 if(cursor_move(1)){
                     reader_move(1);
+                    window_update(&(sh.winfo));
                 }
                 break;
             
